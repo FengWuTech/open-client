30992e0bd81bd7badd33ea1da4fd24c268f17edb
douningbo <douningbo@aiforward.com>
2020-05-29 17:35:50 +0800

    up

--- vision.go
+++ vision.go
@@ -49,7 +49,51 @@ func (cli *Client) ReportWarning(param WarningOptions) (error, Response) {
 	var urlResp Response
 	request := gorequest.New()
 	_, _, errs := request.Post(rawURL).Send(paramStr).EndStruct(&urlResp)
-	if errs != nil || len(errs) > 0 {
+	if len(errs) > 0 {
+		return errs[0], urlResp
+	}
+	return nil, urlResp
+}
+
+
+
+type WarningProcessResultOptions struct {
+	CommunityID int `json:"community_id"  valid:"Required"`  //小区ID
+	DeviceID string `json:"device_id"  valid:"Required"`  //设备编号
+	AlarmID string `json:"alarm_id"  valid:"Required"`  //报警id(按照该id去sync处理结果)
+
+	ProcessName string `json:"process_name"  valid:"Required"`  //处理人姓名
+	ProcessStatus int `json:"process_status"  valid:"Range(1, 4)"`  //处理状态 1 未处理 2 系统自动处理 3 人工处理 4 误报
+}
+
+
+//报警处理结果sync
+func (cli *Client) SyncWarningProcessResult(param WarningProcessResultOptions) (error, Response) {
+
+	path := "/fpms/open/vision/result"
+	query := cli.genQuery(nil)
+	rawURL := cli.BaseURL + path + "?" + query
+
+	// param check
+	valid := validation.Validation{}
+	b, err := valid.Valid(&param)
+	if err != nil {
+		return err,Response{}
+	}
+	if !b {
+		for _, err := range valid.Errors {
+			log.Println(err.Key, err.Message)
+		}
+		return errors.New("参数错误"),Response{}
+	}
+
+	paramJSON, _ := json.Marshal(param)
+	paramStr := string(paramJSON)
+
+	var urlResp Response
+	request := gorequest.New()
+	_, _, errs := request.Post(rawURL).Send(paramStr).EndStruct(&urlResp)
+	if len(errs) > 0 {
 		return errs[0], urlResp
 	}
 	return nil, urlResp
--- vision_test.go
+++ vision_test.go
@@ -34,3 +34,22 @@ func TestReportWarning(t *testing.T) {
 	}
 
 }
+
+
+//测试警报结果通知
+func TestSyncWarningResult(t *testing.T) {
+
+	param := WarningProcessResultOptions {
+		CommunityID :2,
+		DeviceID:"5309ee0e3fb14296b17d962055c07dcf",
+		AlarmID:"h3ai34ye",
+		ProcessName:"张晓华",
+		ProcessStatus:1,
+	}
+
+	err, result := New().SyncWarningProcessResult(param)
+	if err!=nil || result.Code != 200 {
+		t.Errorf("request failed, code[%v]msg[%v]", result.Code, result.Msg)
+	}
+
+}
